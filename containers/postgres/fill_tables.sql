4dbbc70926d5934d970f4ee54e1802761a57acd6
B1Z1 <Ilyamishkin_3211>
2022-01-10 00:12:23 +0100

    Create database for first project

---
+++ containers/postgres/fill_tables.sql
@@ -0,0 +1,21 @@
+INSERT INTO hobby (name)
+VALUES ('Sport');
+
+INSERT INTO hobby (name)
+VALUES ('Programming');
+
+INSERT INTO hobby (name)
+VALUES ('Reading');
+
+INSERT INTO hobby (name)
+VALUES ('Jogging');
+
+INSERT INTO degree (name)
+VALUES ('High');
+
+INSERT INTO degree (name)
+VALUES ('Middle');
+
+INSERT INTO degree (name)
+VALUES ('Primary');
+
