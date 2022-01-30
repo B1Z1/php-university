5b9811f255627864d61accd4592a897f804eb669
B1Z1 <Ilyamishkin_3211>
2022-01-30 20:45:41 +0100

    Product page

--- src/products.php
+++ src/products.php
@@ -16,8 +16,31 @@ $products = $productController->getAll();
 </head>
 <body>
 
-<div class="container pt-6">
-    <h1 class="up-text-center title">Lista produktów</h1>
+<div class="container is-fullheight pt-6">
+    <h1 class="up-text-center title mb-6">Lista produktów</h1>
+
+    <dl>
+        <?php foreach ($products as $product) { ?>
+            <div class="column is-half mx-auto">
+                <dt class="mb-4">
+                    <h4 class="subtitle">
+                        <b><?php echo $product->name; ?></b>
+                    </h4>
+                </dt>
+
+                <dd class="mb-4">
+                    <?php echo $product->description; ?>
+                </dd>
+
+                <div class="is-flex is-align-items-center is-justify-content-end">
+                    <span class="mr-4">
+                        <?php echo $product->price; ?>zł
+                    </span>
+                    <button class="button is-primary">Do koszyka</button>
+                </div>
+            </div>
+        <?php } ?>
+    </dl>
 </div>
 
 <?php include 'base/footer.php'; ?>
